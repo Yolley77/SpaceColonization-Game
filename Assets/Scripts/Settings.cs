7a58eb77f3eaa37bebd023f0a00f752fb5f0e466
Yo <ruslan5-5@ya.ru>
2018-07-10 10:36:01 +0000

    Project

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour {
+    public AudioMixer sounds;
+    public void soundsVolume(float sliderValue)
+    {
+        sounds.SetFloat("masterVolume", sliderValue);
+    }
+
+    public AudioMixer music;
+    public void musicVolume(float sliderValue)
+    {
+        music.SetFloat("musicVolume", sliderValue);
+    }
+}
